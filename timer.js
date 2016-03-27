7379b50d536655c839f328461f032b67b2813d0c
jameswang14 <sambob12@gmail.com>
2016-03-26 20:46:32 -0400

    inserted timer

--- timer.js
+++ timer.js
@@ -1,8 +1,30 @@
-console.time("Time spent on tab:");
+var time = 0;
+function run(url) {
+  if(localStorage.getItem(url))
+    time = localStorage.getItem(url);
+  setInterval(function(){
+    time++; 
+    var timer = document.getElementById("timer");
+    timer.innerHTML = time;
+    localStorage.setItem(url, time);
+  }, 1000); 
+
+}
+document.addEventListener('DOMContentLoaded', function(){
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    var url = tabs[0].url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+    var url_div = document.getElementById("url");
+    url_div.innerHTML = url;
+    run(url);
+  }); 
+
+});
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var viewTabUrl = chrome.extension.getURL('timer.html');
+  //var viewTabUrl = chrome.extension.getURL(tab.);
+  console.log("Hello");
   console.timeEnd("Time spent on tab:");
+
   //var imageUrl = /* an image's URL */;
 
   // Look through all the pages in this extension to find one we can use.
